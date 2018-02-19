6e244d7fe2ea285b015f266affabbf672a5a5c53
Parshva Shah <Parshva Shah>
2018-02-18 21:30:56 -0500

    Added DriveTrain subsystem

---
+++ FEHRobot/Subsystems/DriveTrain.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "FEHMotor.h"
+#include "FEHIO.h"
+
+class DriveTrain
+{
+private:
+	int const MOTOR_VOLTAGE = 7.2;
+	int const PI = 3.14159265358;
+
+	FEHMotor::FEHMotorPort FLMotorPort = FEHMotor::FEHMotorPort::Motor0;
+	FEHMotor::FEHMotorPort FRMotorPort = FEHMotor::FEHMotorPort::Motor1;
+	FEHMotor::FEHMotorPort BLMotorPort = FEHMotor::FEHMotorPort::Motor2;
+	FEHMotor::FEHMotorPort BRMotorPort = FEHMotor::FEHMotorPort::Motor3;
+
+	FEHIO::FEHIOPin FrontLimitPort = FEHIO::FEHIOPin::P0_4;
+
+	FEHMotor FLMotor = FEHMotor(FLMotorPort, MOTOR_VOLTAGE);
+	FEHMotor FRMotor = FEHMotor(FRMotorPort, MOTOR_VOLTAGE);
+	FEHMotor BLMotor = FEHMotor(BLMotorPort, MOTOR_VOLTAGE);
+	FEHMotor BRMotor = FEHMotor(BRMotorPort, MOTOR_VOLTAGE);
+
+	DigitalInputPin FrontLimit = DigitalInputPin(FrontLimitPort);
+public:
+	DriveTrain();
+
+	int driveTilBump();
+	int drive(int heading, int power);
+	int stop();
+};
+
