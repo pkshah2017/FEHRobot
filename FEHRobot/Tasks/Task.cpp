c446b8ee3ecd9f92684b94a39fecc0b53e74625e
Parshva Shah <pkshah2017@gmail.com>
2018-02-23 16:21:50 -0500

    Finished Task abstract class
    Implmented addCommand to Task class
    Changed commandList in Task class to stack

--- FEHRobot/Tasks/Task.cpp
+++ FEHRobot/Tasks/Task.cpp
@@ -5,8 +5,19 @@ Task::Task()
 }
 
 int Task::execute() {
+    int errorCode = 0;
+    while(!commandStack.empty() && errorCode == 0){
+        Command* toRun = commandStack.pop();
+        errorCode = (*toRun).execute();
+        if(errorCode != 0){
+            errorCode = commanFailureRecovery(errorCode);
+        }
+    }
+    return errorCode;
 }
 
 int Task::addCommand(Command* command){
+    commandStack.push(command);
 
+    return 0;
 }
