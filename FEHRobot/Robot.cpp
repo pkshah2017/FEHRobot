aee036b61020804810a0212ea300984ea6df9c42
Parshva Shah <pkshah2017@gmail.com>
2018-02-25 11:36:02 -0500

    Removed old commands from Robot class
    Removed commands added to Robot class for initial testing

--- FEHRobot/Robot.cpp
+++ FEHRobot/Robot.cpp
@@ -1,58 +1,8 @@
 #include "Robot.h"
 #include <FEHUtility.h>
 
-#define REFRESH_RATE 20
-
 Robot::Robot()
 {
-}
-
-
-int Robot::driveTilFrontBump(int heading, int power) {
-    driveTrain.drive(heading, power);
-    while (sensors.getFrontLimit()){
-        Sleep(REFRESH_RATE);
-        sensors.updateSensorStates();
-    }
-    driveTrain.stop();
-    return 0;
-}
-
-int Robot::driveTilLeftBump(int heading, int power) {
-    driveTrain.drive(heading, power);
-    while (sensors.getLeftLimit()){
-        Sleep(REFRESH_RATE);
-        sensors.updateSensorStates();
-    }
-    driveTrain.stop();
-    return 0;
-}
-
-int Robot::driveTilRightBump(int heading, int power) {
-    driveTrain.drive(heading, power);
-    while (sensors.getRightLimit()){
-        Sleep(REFRESH_RATE);
-        sensors.updateSensorStates();
-    }
-    driveTrain.stop();
-    return 0;
-}
-
-int Robot::driveForTime(int heading, int power, int ms) {
-    driveTrain.drive(heading, power);
-    Sleep(ms);
-    driveTrain.stop();
-}
-
-int Robot::driveForTime(int heading, int power, float sec) {
-    driveForTime(heading, power, sec*1000);
-}
-
-int Robot::waitTilStartLight(){
-    do{
-        Sleep(REFRESH_RATE);
-        sensors.updateSensorStates();
-    } while(sensors.getCDSState() < 1.2);
 }
 
 int Robot::updateSensorStates() {
@@ -75,7 +25,6 @@ bool Robot::getLimit(Direction direction) {
 
     return result;
 }
-
 
 float Robot::getCDSState(){
     return sensors.getCDSState();
--- FEHRobot/Robot.h
+++ FEHRobot/Robot.h
@@ -11,13 +11,7 @@ private:
 	Sensors sensors;
 public:
 
-	Robot();
-	int driveTilFrontBump(int heading, int power);
-	int driveTilLeftBump(int heading, int power);
-	int driveTilRightBump(int heading, int power);
-	int driveForTime(int heading, int power, int ms);
-    int driveForTime(int heading, int power, float sec);
-    int waitTilStartLight();
+    Robot();
 
     int updateSensorStates();
 
