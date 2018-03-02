d33cd11a4cfa788416f5af00d8545ea04d088c3c
Parshva Shah <pkshah2017@gmail.com>
2018-03-02 14:26:05 -0500

    Created CommandList with all command variables
    Created CommandList with all command variables

---
+++ FEHRobot/Commands/CommandList.cpp
@@ -0,0 +1,25 @@
+#include "Commands/CenterOnLine.h"
+#include "Commands/ChangeArmPosition.h"
+#include "Commands/DriveForTime.h"
+#include "Commands/DriveTilBump.h"
+#include "Commands/DriveToLine.h"
+#include "Commands/DriveToPosition.h"
+#include "Commands/LineFollowForTime.h"
+#include "Commands/TurnForTime.h"
+#include "Commands/WaitForLight.h"
+#include "Commands/WaitForTime.h"
+
+#include "Commands/MoveArmButtons.h"
+
+extern CenterOnLine centerOnLine;
+extern ChangeArmPosition changeArmPosition;
+extern DriveForTime driveForTime;
+extern DriveTilBump driveTilBump;
+extern DriveToLine driveToLine;
+extern DriveToPosition driveToPosition;
+extern LineFollowForTime lineFollowForTime;
+extern TurnForTime turnForTime;
+extern WaitForLight waitForLight;
+extern WaitForTime waitForTime;
+
+extern MoveArmButtons moveArmButtons;
